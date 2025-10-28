7afc58888d96bcccd60ea9140ec972e80e457bfb
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-10-28 18:15:29 +0300

    feat: search init

---
+++ lib/presentation/screen/search/widget/range_slider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RangeSlider extends StatefulWidget {
+  const RangeSlider({super.key});
+
+  @override
+  State<RangeSlider> createState() => _RangeSliderState();
+}
+
+class _RangeSliderState extends State<RangeSlider> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
