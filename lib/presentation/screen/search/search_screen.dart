7805448010e5747edd27bd857b7ae6b45c149c66
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-10-29 02:39:42 +0300

    feat: search screen price filter

--- lib/presentation/screen/search/search_screen.dart
+++ lib/presentation/screen/search/search_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:share_space/presentation/screen/search/widget/range_chip.dart';
+import 'package:share_space/presentation/screen/search/widget/range_slider.dart';
+
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -8,10 +11,44 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  RangeValues _currentRange = const RangeValues(100, 500);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Column(
+      children: [
+        RangeSliderWidget(
+          maxRange: const RangeValues(0, 1000),
+          currentRange: _currentRange,
+          onChange: (newRange) {
+            setState(() {
+              _currentRange = newRange;
+            });
+          },
+          currency: "IQD",
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: RangeChip(
+                label: "Min",
+                value: _currentRange.start.toInt(),
+                image: "assets/icons/arrow_down.svg",
+                unit: "IQD",
+              ),
+            ),
+            const SizedBox(width: 16),
+            Expanded(
+              child: RangeChip(
+                label: "Max",
+                value: _currentRange.end.toInt(),
+                image: "assets/icons/arrow_down.svg",
+                unit: "IQD",
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
