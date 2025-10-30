1ea7ae149051138b9ee2dc4c373150305ed382d3
Doaa Abdelnaser <doaaabdelnaser151998@gmail.com>
2025-10-31 00:35:25 +0200

    add room details header and services

--- lib/presentation/screen/room_details/widgets/room_details_body.dart
+++ lib/presentation/screen/room_details/widgets/room_details_body.dart
@@ -1,31 +1,48 @@
-
 import 'package:flutter/material.dart';
+import 'package:share_space/presentation/screen/room_details/widgets/room_header_section.dart';
 import 'package:share_space/presentation/screen/room_details/widgets/room_image_slider.dart';
 
-class RoomDetailsBody extends StatelessWidget{
-   RoomDetailsBody({super.key});
+class RoomDetailsBody extends StatelessWidget {
+  RoomDetailsBody({super.key});
+
   final List<String> roomImages = const [
     'assets/images/room_image.png',
     'assets/images/room_image.png',
     'assets/images/room_image.png',
     'assets/images/room_image.png',
-
-
+  ];
+  final ValueNotifier<int> _currentIndex = ValueNotifier(0);
+  final String roomName = 'Modern study room';
+  final String location = 'Baghdad, Iraq';
+  final List<Service> services = [
+    Service(name: 'WiFi',imagePath: 'assets/icons/wifi.svg'),
+    Service(name: 'whiteboard',imagePath: 'assets/icons/whiteboard.svg'),
+    Service(name: 'power backup',imagePath: 'assets/icons/power_backup.svg'),
+    Service(name: 'A/C',imagePath: 'assets/icons/ac.svg'),
   ];
 
-  final ValueNotifier<int> _currentIndex = ValueNotifier(0);
   @override
   Widget build(BuildContext context) {
     final pageController = PageController();
 
     return CustomScrollView(
-     slivers: [
-       SliverToBoxAdapter(child: RoomImageSlider(images: roomImages,currentIndex: _currentIndex,controller: pageController,rate: '5',),),
-
-     ]
-   );
-
+      slivers: [
+        SliverToBoxAdapter(
+          child: RoomImageSlider(
+            images: roomImages,
+            currentIndex: _currentIndex,
+            controller: pageController,
+            rate: '5',
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: RoomHeaderSection(
+            title: roomName,
+            location: location,
+            services: services,
+          ),
+        ),
+      ],
+    );
   }
-
-
 }
