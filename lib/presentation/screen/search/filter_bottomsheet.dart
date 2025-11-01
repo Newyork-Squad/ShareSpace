1b81582703e6c1c61c602d7c3bbf60b71331dd5b
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-11-01 02:18:11 +0200

    feat: wrap filter with base bottom sheet

--- lib/presentation/screen/search/filter_bottomsheet.dart
+++ lib/presentation/screen/search/filter_bottomsheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:share_space/presentation/design_system/theme/app_theme.dart';
+import 'package:share_space/presentation/design_system/widget/base_bottom_sheet.dart';
 import 'package:share_space/presentation/screen/search/widget/range_chip.dart';
 import 'package:share_space/presentation/screen/search/widget/range_slider.dart';
 import 'package:share_space/presentation/screen/search/widget/rating_filter.dart';
@@ -31,154 +32,159 @@ class _FilterBottomSheetState extends State<FilterBottomSheet> {
   @override
   Widget build(BuildContext context) {
     final theme = AppTheme.of(context);
-    return Container(
-      color: theme.colors.surfaceLow,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-        child: Column(
-          children: [
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Rate",
-                style: theme.typography.textTheme.titleSmall?.copyWith(
-                  color: theme.colors.title,
-                ),
-              ),
-            ),
-            SizedBox(height: 12),
-            RatingFilter(
-              options: rateOptions,
-              selectedIndices: selectedRateIndices,
-              onSelect: (index) {
-                setState(() {
-                  if (selectedRateIndices.contains(index)) {
-                    selectedRateIndices.remove(index);
-                    if (selectedRateIndices.isEmpty) {
-                      selectedRateIndices.add(0);
-                    }
-                  } else {
-                    if (index == 0) {
-                      selectedRateIndices.clear();
-                    }
-                    if (selectedRateIndices.length == 1 &&
-                        selectedRateIndices.first == 0) {
-                      selectedRateIndices.clear();
-                    }
-                    selectedRateIndices.add(index);
-                  }
-                });
-              },
-            ),
-            SizedBox(height: 16),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Price Range",
-                style: theme.typography.textTheme.titleSmall?.copyWith(
-                  color: theme.colors.title,
-                ),
-              ),
-            ),
-            SizedBox(height: 12),
-            RangeSliderWidget(
-              maxRange: const RangeValues(0, 1000),
-              currentRange: _currentRange,
-              onChange: (newRange) {
-                setState(() {
-                  _currentRange = newRange;
-                });
-              },
-              currency: "IQD",
-            ),
-            SizedBox(height: 20),
-            Row(
-              children: [
-                Expanded(
-                  child: RangeChip(
-                    label: "Min",
-                    value: _currentRange.start.toInt(),
-                    image: "assets/icons/arrow_down.svg",
-                    unit: "IQD",
+    return BaseBottomSheet(
+      label: "Filter",
+      onClose: (){},
+      child: Container(
+        color: theme.colors.surfaceLow,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Rate",
+                  style: theme.typography.textTheme.titleSmall?.copyWith(
+                    color: theme.colors.title,
                   ),
                 ),
-                const SizedBox(width: 16),
-                Expanded(
-                  child: RangeChip(
-                    label: "Max",
-                    value: _currentRange.end.toInt(),
-                    image: "assets/icons/arrow_down.svg",
-                    unit: "IQD",
+              ),
+              SizedBox(height: 12),
+              RatingFilter(
+                options: rateOptions,
+                selectedIndices: selectedRateIndices,
+                onSelect: (index) {
+                  setState(() {
+                    if (selectedRateIndices.contains(index)) {
+                      selectedRateIndices.remove(index);
+                      if (selectedRateIndices.isEmpty) {
+                        selectedRateIndices.add(0);
+                      }
+                    } else {
+                      if (index == 0) {
+                        selectedRateIndices.clear();
+                      }
+                      if (selectedRateIndices.length == 1 &&
+                          selectedRateIndices.first == 0) {
+                        selectedRateIndices.clear();
+                      }
+                      selectedRateIndices.add(index);
+                    }
+                  });
+                },
+              ),
+              SizedBox(height: 16),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Price Range",
+                  style: theme.typography.textTheme.titleSmall?.copyWith(
+                    color: theme.colors.title,
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 16),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Services",
-                style: theme.typography.textTheme.titleSmall?.copyWith(
-                  color: theme.colors.title,
                 ),
               ),
-            ),
-            SizedBox(height: 12),
-            ServicesFilter(
-              options: servicesOptions,
-              selectedIndices: selectedServicesIndices,
-              onSelect: (index) {
-                setState(() {
-                  if (selectedServicesIndices.contains(index)) {
-                    selectedServicesIndices.remove(index);
-                    if (selectedServicesIndices.isEmpty) {
-                      selectedServicesIndices.add(0);
-                    }
-                  } else {
-                    if (index == 0) {
-                      selectedServicesIndices.clear();
-                    }
-                    if (selectedServicesIndices.length == 1 &&
-                        selectedServicesIndices.first == 0) {
-                      selectedServicesIndices.clear();
-                    }
-                    selectedServicesIndices.add(index);
-                  }
-                });
-              },
-            ),
-            SizedBox(height: 16),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Location",
-                style: theme.typography.textTheme.titleSmall?.copyWith(
-                  color: theme.colors.title,
+              SizedBox(height: 12),
+              RangeSliderWidget(
+                maxRange: const RangeValues(0, 1000),
+                currentRange: _currentRange,
+                onChange: (newRange) {
+                  setState(() {
+                    _currentRange = newRange;
+                  });
+                },
+                currency: "IQD",
+              ),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Expanded(
+                    child: RangeChip(
+                      label: "Min",
+                      value: _currentRange.start.toInt(),
+                      image: "assets/icons/arrow_down.svg",
+                      unit: "IQD",
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: RangeChip(
+                      label: "Max",
+                      value: _currentRange.end.toInt(),
+                      image: "assets/icons/arrow_up.svg",
+                      unit: "IQD",
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Services",
+                  style: theme.typography.textTheme.titleSmall?.copyWith(
+                    color: theme.colors.title,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 12),
-            // TODO: Add Location Filter Widget here
-            SizedBox(height: 24),
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Expanded(
-                  child: SecondaryButton(
-                    onPressed: () {},
-                    label: "Clear",
-                    isDisabled: true,
+              SizedBox(height: 12),
+              ServicesFilter(
+                options: servicesOptions,
+                selectedIndices: selectedServicesIndices,
+                onSelect: (index) {
+                  setState(() {
+                    if (selectedServicesIndices.contains(index)) {
+                      selectedServicesIndices.remove(index);
+                      if (selectedServicesIndices.isEmpty) {
+                        selectedServicesIndices.add(0);
+                      }
+                    } else {
+                      if (index == 0) {
+                        selectedServicesIndices.clear();
+                      }
+                      if (selectedServicesIndices.length == 1 &&
+                          selectedServicesIndices.first == 0) {
+                        selectedServicesIndices.clear();
+                      }
+                      selectedServicesIndices.add(index);
+                    }
+                  });
+                },
+              ),
+              SizedBox(height: 16),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Location",
+                  style: theme.typography.textTheme.titleSmall?.copyWith(
+                    color: theme.colors.title,
                   ),
                 ),
-                Expanded(
-                  child: PrimaryButton(
-                    onPressed: () {},
-                    label: "Apply Filters",
+              ),
+              SizedBox(height: 12),
+              // TODO: Add Location Filter Widget here
+              SizedBox(height: 24),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    child: SecondaryButton(
+                      onPressed: () {},
+                      label: "Clear",
+                      isDisabled: true,
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: PrimaryButton(
+                      onPressed: () {},
+                      label: "Apply Filters",
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
