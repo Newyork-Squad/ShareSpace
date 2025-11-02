d8896e3f91c4e53babfa4cf6c7837a9189f875e6
Tasneem AbdElhakeem <tasniim.hakiim@gmail.com>
2025-11-02 20:53:58 +0200

    feat: implement search screen layout

--- lib/presentation/screen/search/search_screen.dart
+++ lib/presentation/screen/search/search_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:share_space/presentation/screen/search/widget/range_chip.dart';
-import 'package:share_space/presentation/screen/search/widget/range_slider.dart';
-
+import 'package:share_space/presentation/screen/search/widget/search_field.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -11,43 +9,14 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  RangeValues _currentRange = const RangeValues(100, 500);
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        RangeSliderWidget(
-          maxRange: const RangeValues(0, 1000),
-          currentRange: _currentRange,
-          onChange: (newRange) {
-            setState(() {
-              _currentRange = newRange;
-            });
-          },
-          currency: "IQD",
-        ),
-        Row(
-          children: [
-            Expanded(
-              child: RangeChip(
-                label: "Min",
-                value: _currentRange.start.toInt(),
-                image: "assets/icons/arrow_down.svg",
-                unit: "IQD",
-              ),
-            ),
-            const SizedBox(width: 16),
-            Expanded(
-              child: RangeChip(
-                label: "Max",
-                value: _currentRange.end.toInt(),
-                image: "assets/icons/arrow_down.svg",
-                unit: "IQD",
-              ),
-            ),
-          ],
-        ),
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(child: SearchField()),
+        SliverToBoxAdapter(child: Container(height: 500, color: Colors.red)),
+        SliverToBoxAdapter(child: Container(height: 500, color: Colors.black)),
+        SliverToBoxAdapter(child: Container(height: 500, color: Colors.blue)),
       ],
     );
   }
