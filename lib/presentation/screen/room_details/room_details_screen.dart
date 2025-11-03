41038707c903915d39423c3c810830818ddbeab0
Tasneem AbdElhakeem <tasniim.hakiim@gmail.com>
2025-11-03 21:47:13 +0200

    feat: add initial screens structure

--- lib/presentation/screen/room_details/room_details_screen.dart
+++ lib/presentation/screen/room_details/room_details_screen.dart
@@ -1,19 +1,17 @@
-
 import 'package:flutter/material.dart';
 import 'package:share_space/presentation/screen/room_details/widgets/room_booking_bar.dart';
 import 'package:share_space/presentation/screen/room_details/widgets/room_details_body.dart';
 
-class RoomDetailsScreen extends StatelessWidget{
-  final String price='12,000 IQD/h';
-  const RoomDetailsScreen({super.key});
+class RoomDetailsScreen extends StatelessWidget {
+  final String price = '12,000 IQD/h';
 
+  const RoomDetailsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     body: RoomDetailsBody(),
-     bottomNavigationBar:RoomBookingBar(price: price,),
-   );
+    return Scaffold(
+      body: RoomDetailsBody(),
+      bottomNavigationBar: RoomBookingBar(price: price),
+    );
   }
-  
 }
