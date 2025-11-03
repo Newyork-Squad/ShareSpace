66030f101b209f4465629740f56e73b56069d25a
Alaa <alaakhaled9191@gmail.com>
2025-11-03 23:17:02 +0200

    close the keyboard when tapping outside input fields for better accessibility and ux

--- lib/presentation/screen/login/login_screen.dart
+++ lib/presentation/screen/login/login_screen.dart
@@ -17,7 +17,6 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final phoneController = TextEditingController();
   final passwordController = TextEditingController();
-
   final ValueNotifier<bool> isFormValid = ValueNotifier(false);
 
   void _validateForm() {
@@ -46,72 +45,80 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.light.surfaceLow,
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Image.asset(
-              'assets/images/login_screen_shapes.png',
-              fit: BoxFit.cover,
-            ),
-          ),
-          SafeArea(
-            child: Column(
-              children: [
-                Expanded(
-                  child: SingleChildScrollView(
-                    padding: const EdgeInsets.only(left: 16, right: 16, top: 24),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        const AppLogo(),
-                        const SizedBox(height: 24),
-                        Text(
-                          "Welcome Back",
-                          style: AppTypography().textTheme.titleMedium?.copyWith(
-                            color: AppColors.light.title,
-                          ),
-                        ),
-                        const SizedBox(height: 4),
-                        Text(
-                          "Please enter your phone number and password to access your booking",
-                          textAlign: TextAlign.center,
-                          style: AppTypography().textTheme.bodyMedium?.copyWith(
-                            color: AppColors.light.body,
-                          ),
-                        ),
-                        const SizedBox(height: 32),
-                        PhoneInputField(controller: phoneController),
-                        const SizedBox(height: 16),
-                        PasswordInputField(controller: passwordController),
-                      ],
-                    ),
-                  ),
-                ),
 
-                ValueListenableBuilder<bool>(
-                  valueListenable: isFormValid,
-                  builder: (context, valid, _) {
-                    return Container(
-                      color: AppColors.light.surfaceLow.withOpacity(0.9),
-                      padding: const EdgeInsets.only(left: 16, right: 16, bottom: 12),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        behavior: HitTestBehavior.translucent,
+
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Image.asset(
+                'assets/images/login_screen_shapes.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+            SafeArea(
+              child: Column(
+                children: [
+                  Expanded(
+                    child: SingleChildScrollView(
+                      padding: const EdgeInsets.only(left: 16, right: 16, top: 24),
                       child: Column(
-                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          LoginButton(
-                            isEnabled: valid,
-                            onPressed: valid ? () {} : null,
+                          const AppLogo(),
+                          const SizedBox(height: 24),
+                          Text(
+                            "Welcome Back",
+                            style: AppTypography().textTheme.titleMedium?.copyWith(
+                              color: AppColors.light.title,
+                            ),
                           ),
-                          const SizedBox(height: 12),
-                          RegisterText(onTap: () {}),
+                          const SizedBox(height: 4),
+                          Text(
+                            "Please enter your phone number and password to access your booking",
+                            textAlign: TextAlign.center,
+                            style: AppTypography().textTheme.bodyMedium?.copyWith(
+                              color: AppColors.light.body,
+                            ),
+                          ),
+                          const SizedBox(height: 32),
+                          PhoneInputField(controller: phoneController),
+                          const SizedBox(height: 16),
+                          PasswordInputField(controller: passwordController),
                         ],
                       ),
-                    );
-                  },
-                ),
-              ],
+                    ),
+                  ),
+
+                  ValueListenableBuilder<bool>(
+                    valueListenable: isFormValid,
+                    builder: (context, valid, _) {
+                      return Container(
+                        color: AppColors.light.surfaceLow.withOpacity(0.9),
+                        padding: const EdgeInsets.only(left: 16, right: 16, bottom: 12),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            LoginButton(
+                              isEnabled: valid,
+                              onPressed: valid ? () {} : null,
+                            ),
+                            const SizedBox(height: 12),
+                            RegisterText(onTap: () {}),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
