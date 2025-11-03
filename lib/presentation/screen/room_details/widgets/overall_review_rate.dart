7490fbd20455120bf2d62ad41abd50110d286e61
Doaa Abdelnaser <doaaabdelnaser151998@gmail.com>
2025-11-03 20:28:18 +0200

    add about room screen and review indicators

---
+++ lib/presentation/screen/room_details/widgets/overall_review_rate.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../design_system/theme/app_theme.dart';
+
+class OverAllReviewRate extends StatelessWidget {
+  const OverAllReviewRate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = AppTheme.of(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          '4.0',
+          style: theme.typography.textTheme.titleMedium?.copyWith(
+            color: theme.colors.title,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        const SizedBox(height: 5),
+        RatingBarIndicator(
+          rating: 5.0,
+          itemCount: 5,
+          itemSize: 16,
+          unratedColor: theme.colors.surfaceLow,
+          direction: Axis.horizontal,
+          itemPadding: const EdgeInsets.symmetric(horizontal: 4),
+          itemBuilder: (context, index) {
+            return SvgPicture.asset(
+              index < 3
+                  ? 'assets/icons/filled_star.svg'
+                  : 'assets/icons/star_outline.svg',
+              color: theme.colors.yellow,
+            );
+          },
+        ),
+        const SizedBox(height: 5),
+        Text(
+          'based on 122 reviews',
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          style: theme.typography.textTheme.labelSmall?.copyWith(
+            color: theme.colors.hint,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/presentation/screen/room_details/widgets/reviews_header.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../design_system/theme/app_theme.dart';
+
+class ReviewsHeader extends StatelessWidget {
+  const ReviewsHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = AppTheme.of(context);
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          'Reviews (122)',
+          style: theme.typography.textTheme.titleSmall?.copyWith(
+            color: theme.colors.title,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+
+        InkWell(
+          onTap: () {},
+          child: Row(
+            children: [
+              Text(
+                'All',
+                style: theme.typography.textTheme.labelSmall?.copyWith(
+                  color: theme.colors.title,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const SizedBox(width: 8),
+              SvgPicture.asset(
+                'assets/icons/arrow_right.svg',
+                color: theme.colors.title,
+                width: 12,
+                height: 12,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
