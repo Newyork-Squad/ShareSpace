f66e73f48f151aa84df8d14ae1875732f8bd5ce9
Alaa <alaakhaled9191@gmail.com>
2025-11-04 00:06:29 +0200

    fixed problem with stars move up and down when typing

--- lib/presentation/screen/login/login_screen.dart
+++ lib/presentation/screen/login/login_screen.dart
@@ -45,6 +45,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: AppColors.light.surfaceLow,
 
       body: GestureDetector(
