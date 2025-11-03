2831710c66ad07547000629b6d3f7bfb8e535fd3
Alaa <alaakhaled9191@gmail.com>
2025-11-03 22:06:26 +0200

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:share_space/presentation/design_system/colors/app_color.dart';
 import 'package:share_space/presentation/design_system/theme/app_theme.dart';
 import 'package:share_space/presentation/design_system/typography/app_typography.dart';
-import 'package:share_space/presentation/screen/room_details/room_details_screen.dart';
+import 'package:share_space/presentation/screen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,15 +11,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return AppTheme(
       colors: AppColors.light,
       typography: AppTypography(),
-    child: MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const RoomDetailsScreen(),
-    ));
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashScreen(),
+      ),
+    );
   }
 }
