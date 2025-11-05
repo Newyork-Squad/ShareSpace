d24ebe459c46ca057e4e3ace38097412bc707a08
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-11-05 23:05:39 +0200

    feat: category filter row

--- lib/presentation/screen/home/home_screen.dart
+++ lib/presentation/screen/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:share_space/presentation/screen/home/widget/booking_card.dart';
+import 'package:share_space/presentation/screen/home/widget/category_chip.dart';
 import 'package:share_space/presentation/screen/home/widget/home_app_bar.dart';
 
 import '../../design_system/theme/app_theme.dart';
@@ -14,7 +15,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   String tempImage =
       "https://images.unsplash.com/photo-1579546929518-9e396f3cc809?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-  List<String> categories = ["All", "Rooms", "Apartments", "Offices"];
+  List<String> categories = ["All", "Rooms", "Apartments", "Offices", "Hotels", "Hostels", "Resorts", "Guest Houses", "Guest Homes"];
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -46,8 +48,8 @@ class _HomeScreenState extends State<HomeScreen> {
                         return Padding(
                           padding: const EdgeInsets.fromLTRB(12, 16, 4, 28),
                           child: BookingCard(
-                            label: 'slekfmkls${categories[index]}',
-                            description: 'fklzsemklfzm',
+                            label: categories[index],
+                            description: 'wow room',
                             imageUrl: tempImage,
                           ),
                         );
@@ -55,6 +57,28 @@ class _HomeScreenState extends State<HomeScreen> {
                     );
                   }
                 },
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 40,
+              child: ListView.separated(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                scrollDirection: Axis.horizontal,
+                itemCount: categories.length,
+                itemBuilder: (context, index) {
+                  return CategoryChip(
+                    label: categories[index],
+                    isSelected: _selectedIndex == index,
+                    onTap: () {
+                      setState(() {
+                        _selectedIndex = index;
+                      });
+                    },
+                  );
+                },
+                separatorBuilder: (context, index) => const SizedBox(width: 8),
               ),
             ),
           ),
