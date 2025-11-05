42faf083c16c20f23704ab849129bf352845f572
Tasneem AbdElhakeem <tasniim.hakiim@gmail.com>
2025-11-05 13:38:34 +0200

    refactor: resolve merge conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:share_space/presentation/design_system/colors/app_color.dart';
-import 'package:share_space/presentation/design_system/theme/app_theme.dart';
-import 'package:share_space/presentation/design_system/typography/app_typography.dart';
-import 'package:share_space/presentation/screen/room_details/about_room_screen.dart';
-import 'package:share_space/presentation/screen/room_details/all_reviews_screen.dart';
-import 'package:share_space/presentation/screen/room_details/room_details_screen.dart';
 
 import 'presentation/design_system/theme/app_theme_provider.dart';
-import 'presentation/screen/my_account/my_account_screen.dart';
+import 'presentation/routes/app_router.dart';
+import 'presentation/routes/routes.dart';
 
 void main() {
   runApp(ShareSpaceApp(appRouter: AppRouter()));
@@ -17,7 +12,6 @@ class ShareSpaceApp extends StatelessWidget {
   final AppRouter appRouter;
 
   const ShareSpaceApp({super.key, required this.appRouter});
-
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +23,8 @@ class ShareSpaceApp extends StatelessWidget {
           useMaterial3: true,
           scaffoldBackgroundColor: Colors.white,
         ),
-        initialRoute: Routes.appNavigationBar,  // TODO: change it later
+        initialRoute: Routes.appNavigationBar,
+        // TODO: change it later
         onGenerateRoute: appRouter.generateRoute,
       ),
     );
