d961eaaa83cfb5e0be920de633ca9d5b2838fff5
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-11-05 21:39:42 +0200

    fix: button gray background

--- lib/presentation/screen/home/widget/booking_card.dart
+++ lib/presentation/screen/home/widget/booking_card.dart
@@ -20,77 +20,78 @@ class BookingCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = AppTheme.of(context);
     return SizedBox(
-          height: 172,
-          width: 320,
-          child: Stack(
-            children: [
-              Container(
-                height: 160,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(24),
-                  image: DecorationImage(
-                    image: NetworkImage(imageUrl),
-                    fit: BoxFit.fill,
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 12.0,
-                    vertical: 20,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        label,
-                        style: theme.typography.textTheme.titleMedium?.copyWith(
-                          color: theme.colors.onPrimary,
-                        ),
-                      ),
-                      const SizedBox(height: 0),
-                      Text(
-                        "$description\n",
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: theme.typography.textTheme.bodySmall?.copyWith(
-                          color: theme.colors.onPrimary,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+      height: 180,
+      width: 320,
+      child: Stack(
+        children: [
+          Container(
+            height: 160,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(24),
+              image: DecorationImage(
+                image: NetworkImage(imageUrl),
+                fit: BoxFit.fill,
               ),
-              Positioned(
-                right: 16,
-                bottom: -8,
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: WidgetStateProperty.all(Colors.transparent),
-                  ),
-                  onPressed: onClick,
-                  child: Ink(
-                    width: 90,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      color: theme.colors.primary,
-                      borderRadius: BorderRadius.circular(30),
-                      border: Border.all(color: Colors.white, width: 4),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Book Now',
-                        style: theme.typography.textTheme.labelSmall?.copyWith(
-                          color: theme.colors.onPrimary,
-                        ),
-                      ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 12.0,
+                vertical: 20,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    label,
+                    style: theme.typography.textTheme.titleMedium?.copyWith(
+                      color: theme.colors.onPrimary,
                     ),
                   ),
-                ),
+                  const SizedBox(height: 0),
+                  Text(
+                    "$description\n",
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: theme.typography.textTheme.bodySmall?.copyWith(
+                      color: theme.colors.onPrimary,
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        );
+          Positioned(
+            right: 16,
+            bottom: -8,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                elevation: WidgetStateProperty.all(0),
+                backgroundColor: WidgetStateProperty.all(Colors.transparent),
+              ),
+              onPressed: onClick,
+              child: Ink(
+                width: 90,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: theme.colors.primary,
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Colors.white, width: 4),
+                ),
+                child: Center(
+                  child: Text(
+                    'Book Now',
+                    style: theme.typography.textTheme.labelSmall?.copyWith(
+                      color: theme.colors.onPrimary,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
