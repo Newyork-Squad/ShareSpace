cc3246a3068a0294e0780928846add1d512819bc
Alaa <alaakhaled9191@gmail.com>
2025-11-06 11:39:36 +0200

    linking the splash screen onboarding screen, and login screen to each other

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:share_space/presentation/design_system/colors/app_color.dart';
-import 'package:share_space/presentation/design_system/theme/app_theme.dart';
-import 'package:share_space/presentation/design_system/typography/app_typography.dart';
-import 'package:share_space/presentation/screen/room_details/room_details_screen.dart';
-
+import 'package:share_space/presentation/screen/splash_screen.dart';
 import 'presentation/design_system/theme/app_theme_provider.dart';
-import 'presentation/screen/my_account/my_account_screen.dart';
 
 void main() {
   runApp(const ShareSpaceApp());
@@ -24,7 +19,7 @@ class ShareSpaceApp extends StatelessWidget {
           useMaterial3: true,
           scaffoldBackgroundColor: Colors.white,
         ),
-        home: const MyAccountScreen(),
+        home: const SplashScreen(),
       ),
     );
   }
--- lib/presentation/screen/splash_screen.dart
+++ lib/presentation/screen/splash_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:share_space/presentation/design_system/colors/app_color.dart';
-import 'package:share_space/presentation/screen/room_details/room_details_screen.dart';
+import 'onboarding/onboarding_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -17,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
     Timer(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const RoomDetailsScreen()),
+        MaterialPageRoute(builder: (context) => const OnboardingScreen()),
       );
     });
   }
