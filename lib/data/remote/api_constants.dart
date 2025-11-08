3445bb973d4485290754636ebadc67b03243ed45
Kareem Mohamed <63722@fa-hiss.edu.eg>
2025-11-08 02:20:49 +0200

    add api constants.

---
+++ lib/data/remote/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  static const String apiBaseUrl =
+      "https://sharespace-backend-2834.onrender.com/api/";
+
+  static const String login = "auth/login";
+  static const String signup = "auth/register";
+}
