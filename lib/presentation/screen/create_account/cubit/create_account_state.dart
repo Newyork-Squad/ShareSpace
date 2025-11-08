3bd245da8cd2f2f0d68f085894dbe98610bb3484
Alaa <alaakhaled9191@gmail.com>
2025-11-08 18:05:41 +0200

    add create account state

---
+++ lib/presentation/screen/create_account/cubit/create_account_state.dart
@@ -0,0 +1,36 @@
+abstract class CreateAccountState {}
+
+class CreateAccountInitial extends CreateAccountState {}
+
+class CreateAccountLoading extends CreateAccountState {}
+
+class CreateAccountSuccess extends CreateAccountState {}
+
+class CreateAccountError extends CreateAccountState {
+  final String message;
+
+  CreateAccountError(this.message);
+}
+
+class CreateAccountValidationError extends CreateAccountState {
+  final String? nameError;
+  final String? phoneError;
+  final String? emailError;
+  final String? passwordError;
+  final String? confirmPasswordError;
+
+  CreateAccountValidationError({
+    this.nameError,
+    this.phoneError,
+    this.emailError,
+    this.passwordError,
+    this.confirmPasswordError,
+  });
+
+  bool get hasErrors =>
+      nameError != null ||
+          phoneError != null ||
+          emailError != null ||
+          passwordError != null ||
+          confirmPasswordError != null;
+}
