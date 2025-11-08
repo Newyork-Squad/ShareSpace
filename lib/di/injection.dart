6528a72942ffb9f927fe9c2b402731daf9bf71c5
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-11-08 15:19:43 +0200

    feat: state

--- lib/di/injection.dart
+++ lib/di/injection.dart
@@ -1,6 +1,12 @@
 import 'package:get_it/get_it.dart';
 import 'package:share_space/data/repository/workspace_repository_impl.dart';
 import 'package:share_space/domain/repository/workspace_repository.dart';
+import 'package:share_space/domain/usecase/workspace/get_best.dart';
+import 'package:share_space/domain/usecase/workspace/get_best_price.dart';
+import 'package:share_space/domain/usecase/workspace/get_near_to_you.dart';
+import 'package:share_space/domain/usecase/workspace/get_popular.dart';
+import 'package:share_space/domain/usecase/workspace/get_top_rated.dart';
+import 'package:share_space/presentation/screen/home/state/home_cubit.dart';
 import '../data/remote/dio_client.dart';
 import '../data/remote/workspace_api_service.dart';
 import '../data/remote/workspace_api_service_impl.dart';
@@ -8,7 +14,6 @@ import '../data/remote/workspace_api_service_impl.dart';
 final getIt = GetIt.instance;
 
 void setupDependencies() {
-
   getIt.registerLazySingleton(() => DioClient.instance);
 
   getIt.registerLazySingleton<WorkspaceApiService>(
@@ -17,5 +22,21 @@ void setupDependencies() {
 
   getIt.registerLazySingleton<WorkspaceRepository>(
     () => WorkspaceRepositoryImpl(getIt()),
+  );
+
+  getIt.registerLazySingleton(() => GetBestUseCase(getIt()));
+  getIt.registerLazySingleton(() => GetBestPriceUseCase(getIt()));
+  getIt.registerLazySingleton(() => GetPopularUseCase(getIt()));
+  getIt.registerLazySingleton(() => GetNearToYouUseCase(getIt()));
+  getIt.registerLazySingleton(() => GetTopRatedUseCase(getIt()));
+
+  getIt.registerFactory(
+    () => HomeCubit(
+      getIt(),
+      getIt(),
+      getIt(),
+      getIt(),
+      getIt(),
+    ),
   );
 }
--- lib/presentation/screen/home/home_screen.dart
+++ lib/presentation/screen/home/home_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:share_space/presentation/screen/home/state/home_cubit.dart';
+import 'package:share_space/presentation/screen/home/state/home_state.dart';
 import 'package:share_space/presentation/screen/home/widget/booking_card.dart';
 import 'package:share_space/presentation/screen/home/widget/category_chip.dart';
 import 'package:share_space/presentation/screen/home/widget/home_app_bar.dart';
 import 'package:share_space/presentation/screen/home/widget/listing_card.dart';
+import 'package:share_space/presentation/screen/home/widget/workspaces_grid.dart';
 
+import '../../../di/injection.dart';
 import '../../design_system/theme/app_theme.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -14,114 +19,128 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  String tempImage =
-      "https://www.erfurt.com/fileadmin/user_upload/tipps-inspirationen/tipps-tricks/raumwirkung/Leerer-Raum-graue-Wand-weisser-streifenunten-weissedecke_620x417px.jpg";
-  List<String> categories = [
-    "All",
-    "Rooms",
-    "Apartments",
-    "Offices",
-    "Hotels",
-    "Hostels",
-    "Resorts",
-    "Guest Houses",
-    "Guest Homes",
-  ];
-  int _selectedIndex = 0;
-
   @override
   Widget build(BuildContext context) {
     final theme = AppTheme.of(context);
-    return Container(
-      color: theme.colors.surfaceLow,
-      child: CustomScrollView(
-        slivers: [
-          HomeAppBar(
-            userName: "Ahmed Hussein",
-            location: "Cairo, Egypt",
-            profileImageUrl: tempImage,
-          ),
-          // Book now section
-          SliverToBoxAdapter(
-            child: SizedBox(
-              height: 172,
-              width: 200,
-              child: FutureBuilder(
-                future: Future.delayed(const Duration(seconds: 2)),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else {
-                    return ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: categories.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Padding(
-                          padding: const EdgeInsets.fromLTRB(12, 16, 4, 28),
-                          child: BookingCard(
-                            label: categories[index],
-                            description: 'wow room',
-                            imageUrl: tempImage,
-                          ),
-                        );
-                      },
-                    );
-                  }
-                },
+    return BlocProvider(
+      create: (context) => getIt<HomeCubit>()..fetchHome(),
+      child: BlocBuilder<HomeCubit, HomeState>(
+        builder: (context, state) {
+          if (state is HomeLoading) {
+            return Center(
+              child: CircularProgressIndicator(
+                color: AppTheme.of(context).colors.primary,
               ),
-            ),
-          ),
-          // Chips row
-          SliverToBoxAdapter(
-            child: SizedBox(
-              height: 40,
-              child: ListView.separated(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                scrollDirection: Axis.horizontal,
-                itemCount: categories.length,
-                itemBuilder: (context, index) {
-                  return CategoryChip(
-                    label: categories[index],
-                    isSelected: _selectedIndex == index,
-                    onTap: () {
-                      setState(() {
-                        _selectedIndex = index;
-                      });
-                    },
-                  );
-                },
-                separatorBuilder: (context, index) => const SizedBox(width: 8),
-              ),
-            ),
-          ),
-          const SliverToBoxAdapter(child: SizedBox(height: 24)),
-          // Cards
-          SliverToBoxAdapter(
-            child: SizedBox(
-              height: 320,
-              child: ListView.separated(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 16,
-                  vertical: 8,
-                ),
-                scrollDirection: Axis.horizontal,
-                itemCount: 5,
-                separatorBuilder: (context, index) => const SizedBox(width: 4),
-                itemBuilder: (context, index) => SizedBox(
-                  width: 328,
-                  child: ListingCard(
-                    imageUrl: tempImage,
-                    rating: 4.5,
-                    title: 'Night Owl Room',
-                    price: '12,000 IQD/h',
-                    location: 'Baghdad, Iraq',
-                    amenities: const ['WiFi', 'A/C', 'Whiteboard'],
+            );
+          } else if (state is HomeLoaded) {
+            return Container(
+              color: theme.colors.surfaceLow,
+              child: CustomScrollView(
+                slivers: [
+                  HomeAppBar(
+                    userName: "Ahmed Hussein",
+                    location: "Cairo, Egypt",
+                    profileImageUrl: "tempImage",
                   ),
-                ),
+                  // Book now section
+                  SliverToBoxAdapter(
+                    child: SizedBox(
+                      height: 172,
+                      width: 200,
+                      child: FutureBuilder(
+                        future: Future.delayed(const Duration(seconds: 2)),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          } else {
+                            return ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: state.categories.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                return Padding(
+                                  padding: const EdgeInsets.fromLTRB(
+                                    12,
+                                    16,
+                                    4,
+                                    28,
+                                  ),
+                                  child: BookingCard(
+                                    label: state.categories[index].name,
+                                    description:
+                                        state.categories[index].description,
+                                    imageUrl: state.categories[index]
+                                        .getImage(),
+                                  ),
+                                );
+                              },
+                            );
+                          }
+                        },
+                      ),
+                    ),
+                  ),
+                  // Chips row
+                  SliverToBoxAdapter(
+                    child: SizedBox(
+                      child: ListView.separated(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        scrollDirection: Axis.horizontal,
+                        itemCount: state.categories.length,
+                        itemBuilder: (context, index) {
+                          return CategoryChip(
+                            label: state.categories[index].name,
+                            isSelected: state.selectedIndex == index,
+                            onTap: () {
+                              setState(() {
+                                state.selectedIndex = index;
+                              });
+                            },
+                          );
+                        },
+                        separatorBuilder: (context, index) =>
+                            const SizedBox(width: 8),
+                      ),
+                    ),
+                  ),
+                  const SliverToBoxAdapter(child: SizedBox(height: 24)),
+                  SliverToBoxAdapter(child: Text(state.categories[state.selectedIndex].name, style: theme.typography.textTheme.titleSmall,)),
+                  const SliverToBoxAdapter(child: SizedBox(height: 12)),
+                  // Cards
+                  SliverToBoxAdapter(
+                    child: SizedBox(
+                      height: 320,
+                      child: ListView.separated(
+                        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                        scrollDirection: Axis.horizontal,
+                        itemCount: state.categories[state.selectedIndex].workspaces.length,
+                        separatorBuilder: (context, index) => const SizedBox(width: 4),
+                        itemBuilder: (context, index) => SizedBox(
+                          width: 328,
+                          child: ListingCard(
+                            imageUrl: state.categories[state.selectedIndex].workspaces[index].imageUrls[0],
+                            rating: state.categories[state.selectedIndex].workspaces[index].rate,
+                            title: state.categories[state.selectedIndex].workspaces[index].name,
+                            price: state.categories[state.selectedIndex].workspaces[index].pricePerHour.toInt().toString(),
+                            location: state.categories[state.selectedIndex].workspaces[index].locationName,
+                            amenities: state.categories[state.selectedIndex].workspaces[index].services
+                                .map((e) => e.name)
+                                .toList(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ),
-        ],
+            );
+          } else if (state is HomeError) {
+            return Center(child: Text(state.message));
+          }
+          return const SizedBox.shrink();
+        },
       ),
     );
   }
