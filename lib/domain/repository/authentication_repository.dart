9d6e70646f73d8d344a6df2613548a7a41d734bb
Alaa <alaakhaled9191@gmail.com>
2025-11-08 18:03:29 +0200

    add create account in repository interface

--- lib/domain/repository/authentication_repository.dart
+++ lib/domain/repository/authentication_repository.dart
@@ -1,3 +1,13 @@
 abstract class AuthenticationRepository {
   Future<bool> login(String username, String password);
+
+  Future<bool> createAccount({
+    required String email,
+    required String password,
+    required String name,
+    required String phoneNumber,
+    required String gender,
+    String? imageUrl,
+    String? bio,
+  });
 }
