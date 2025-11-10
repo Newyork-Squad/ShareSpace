5dd9728aa13f4423f66813c387a92dcd68625920
Doaa Abdelnaser <doaaabdelnaser151998@gmail.com>
2025-11-10 16:28:48 +0200

    fix room details appbar and rating container

--- lib/presentation/screen/room_details/widgets/room_details_appbar.dart
+++ lib/presentation/screen/room_details/widgets/room_details_appbar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:share_space/presentation/design_system/typography/app_typography.dart';
 import '../../../design_system/theme/app_theme.dart';
 
 class RoomDetailsAppbar extends StatelessWidget {
@@ -16,95 +17,87 @@ class RoomDetailsAppbar extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = AppTheme.of(context);
 
-    return Stack(
-      children: [
-        // Back button
-        Positioned(
-          top: 40,
-          left: 16,
-          child: IconButton(
+    return Container(
+      height: 64,
+      padding: const EdgeInsets.only(top: 24,right: 8),
+      color: isScrolled?theme.colors.surfaceLow:Colors.transparent, // or any color you prefer
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          IconButton(
             icon: SvgPicture.asset(
               'assets/icons/arrow_left.svg',
-              color:isScrolled ? theme.colors.onPrimary: theme.colors.title,
               width: 24,
               height: 24,
+              color: isScrolled?theme.colors.title:  theme.colors.onPrimary,
             ),
             onPressed: () {
-              print('back');
+              Navigator.of(context).pop();
             },
           ),
-        ),
+          Positioned(
+            top: 61,
+            left: 162,
+            right: 162,
+            child: Container(
+              width: 36,
+              height: 22,
+              decoration: BoxDecoration(
+                color: isScrolled ?theme.colors.yellowVariant : theme.colors.surfaceLow,
+                borderRadius: BorderRadius.circular(100),
+              ),
 
-        // Bookmark button
-        Positioned(
-          top: 40,
-          right: 16,
-          child: IconButton(
-            icon: SvgPicture.asset(
-              'assets/icons/bookmark.svg',
-              color: isScrolled?theme.colors.onPrimary: theme.colors.title,
-              width: 24,
-              height: 24,
-            ),
-            onPressed: () {
-              print('bookmark');
-            },
-          ),
-        ),
-
-        // Information button
-        Positioned(
-          top: 40,
-          right: 54,
-          child: IconButton(
-            icon: SvgPicture.asset(
-              'assets/icons/information.svg',
-              color: isScrolled?theme.colors.onPrimary: theme.colors.title,
-              width: 24,
-              height: 24,
-            ),
-            onPressed: () {
-              print('information');
-            },
-          ),
-        ),
-
-        // Rating badge
-        Positioned(
-          top: 61,
-          right: 162,
-          child: Container(
-            width: 36,
-            height: 22,
-            decoration: BoxDecoration(
-              color: isScrolled ? theme.colors.surfaceLow: theme.colors.yellowVariant,
-              borderRadius: BorderRadius.circular(100),
-            ),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 4.0),
-                  child: SvgPicture.asset(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
                     'assets/icons/star.svg',
                     color: theme.colors.yellow,
                     width: 12,
                     height: 12,
                   ),
-                ),
-                Text(
-                  rate,
-                  style: theme.typography.textTheme.labelSmall?.copyWith(
-                    color: theme.colors.yellow,
-                    fontWeight: FontWeight.w100,
+
+                  Text(
+
+                    rate,
+                    style:AppTypography.labelXSmall.copyWith(
+                      color: theme.colors.yellow,
+                      fontWeight: FontWeight.w100,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+
+          Row(
+            children: [
+              IconButton(
+                icon: SvgPicture.asset(
+                  'assets/icons/information.svg',
+                  width: 24,
+                  height: 24,
+                  color: isScrolled?theme.colors.title :  theme.colors.onPrimary,
+                ),
+                onPressed: () {},
+              ),
+
+
+              IconButton(
+                icon: SvgPicture.asset(
+                  'assets/icons/bookmark.svg',
+                  width: 24,
+                  height: 24,
+                  color: isScrolled?theme.colors.title:  theme.colors.onPrimary,
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+
+        ],
+      ),
     );
   }
 }
