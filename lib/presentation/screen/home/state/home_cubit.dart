1c9cb4516daf2cb03c2c06d6e71af2f0da0c4432
abdelrahman <abdelrahmankarim7@gmail.com>
2025-11-11 00:24:38 +0200

    feat : link usecase to home screen

--- lib/presentation/screen/home/state/home_cubit.dart
+++ lib/presentation/screen/home/state/home_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:share_space/domain/usecase/User/getUserDetailsUseCase.dart';
+import 'package:share_space/domain/usecase/User/getUserLocationUseCase.dart';
 import 'package:share_space/domain/usecase/workspace/get_best.dart';
 import 'package:share_space/domain/usecase/workspace/get_best_price.dart';
 import 'package:share_space/domain/usecase/workspace/get_near_to_you.dart';
@@ -15,6 +16,7 @@ class HomeCubit extends Cubit<HomeState> {
   final GetNearToYouUseCase _getNearToYouUseCase;
   final GetTopRatedUseCase _getTopRatedUseCase;
   final GetUserDetailsUseCase _getUserDetailsUseCase;
+  final GetCurrentLocationUseCase _getCurrentLocationUseCase;
 
   HomeCubit(
     this._getBestUseCase,
@@ -23,21 +25,42 @@ class HomeCubit extends Cubit<HomeState> {
     this._getNearToYouUseCase,
     this._getTopRatedUseCase,
     this._getUserDetailsUseCase,
+    this._getCurrentLocationUseCase,
   ) : super(HomeInitial());
 
   Future<void> fetchHome() async {
     emit(HomeLoading());
     try {
-      final (best, popular, bestPrice, topRated, nearToYou,user) = await (
+      final (
+        best,
+        popular,
+        bestPrice,
+        topRated,
+        nearToYou,
+        user,
+        location,
+      ) = await (
         _getBestUseCase(),
         _getPopularUseCase(),
         _getBestPriceUseCase(),
         _getTopRatedUseCase(),
         _getNearToYouUseCase(latitude: 0.0, longitude: 0.0),
-      _getUserDetailsUseCase()
+        _getUserDetailsUseCase(),
+        _getCurrentLocationUseCase(),
       ).wait;
 
-      emit(HomeLoaded(best, bestPrice, popular, nearToYou, topRated,user.name,user.profileImageUrl));
+      emit(
+        HomeLoaded(
+          best,
+          bestPrice,
+          popular,
+          nearToYou,
+          topRated,
+          user.name,
+          user.profileImageUrl,
+          location,
+        ),
+      );
     } catch (e) {
       emit(HomeError(e.toString()));
     }
