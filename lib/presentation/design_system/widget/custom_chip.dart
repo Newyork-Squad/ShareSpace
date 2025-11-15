3777531e5a9403cfba45c409fb5474ff5da44dbe
Kareem Mohamed <63722@fa-hiss.edu.eg>
2025-11-15 04:54:03 +0200

    add custom chip ui in design system.

--- lib/presentation/design_system/widget/custom_chip.dart
+++ lib/presentation/design_system/widget/custom_chip.dart
@@ -4,76 +4,42 @@ import 'package:share_space/presentation/design_system/theme/app_theme.dart';
 
 import '../typography/app_typography.dart';
 
-class CustomChip extends StatefulWidget {
+class CustomChip extends StatelessWidget {
   final String label;
-  Color labelColor;
   final String icon;
-  bool isSelected;
-  final VoidCallback? onSelect;
-  final int width;
-  final int height;
+  final Color? labelColor;
 
-  CustomChip({
+  const CustomChip({
     super.key,
     required this.label,
-    this.labelColor = const Color(0x991F1F1F),
     this.icon = '',
-    this.isSelected = false,
-    this.onSelect,
-    this.width = 37,
-    this.height = 22,
+    this.labelColor,
   });
 
   @override
-  State<CustomChip> createState() => _CustomChipState();
-}
-
-class _CustomChipState extends State<CustomChip> {
-  @override
   Widget build(BuildContext context) {
     final theme = AppTheme.of(context);
-    return GestureDetector(
-      onTap: () => setState(() {
-        widget.onSelect?.call();
-      }),
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
-        decoration: BoxDecoration(
-          color: widget.isSelected
-              ? theme.colors.blueVariant
-              : theme.colors.surface,
-          border: Border.all(
-            color: widget.isSelected
-                ? theme.colors.primary
-                : theme.colors.stroke,
-            width: 0.5,
-          ),
-          borderRadius: BorderRadius.circular(100),
-        ),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            if (widget.icon.isNotEmpty) ...[
-              SvgPicture.asset(
-                widget.icon,
-                colorFilter: widget.isSelected
-                    ? ColorFilter.mode(theme.colors.primary, BlendMode.srcIn)
-                    : null,
-                width: 12,
-                height: 12,
-              ),
-              const SizedBox(width: 2),
-            ],
-            Text(
-              widget.label,
-              style: AppTypography.labelXSmall.copyWith(
-                color: widget.isSelected
-                    ? theme.colors.primary
-                    : widget.labelColor,
-              ),
-            ),
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+      decoration: BoxDecoration(
+        color: theme.colors.surface,
+        border: Border.all(color: theme.colors.stroke, width: 0.5),
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (icon.isNotEmpty) ...[
+            SvgPicture.asset(icon, width: 12, height: 12),
+            const SizedBox(width: 2),
           ],
-        ),
+          Text(
+            label,
+            style: AppTypography.labelXSmall.copyWith(
+              color: labelColor ?? theme.colors.body,
+            ),
+          ),
+        ],
       ),
     );
   }
