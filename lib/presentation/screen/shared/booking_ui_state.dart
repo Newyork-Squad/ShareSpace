1c9df6da6878bf139061abe373d2c621454e3aba
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-11-22 14:44:00 +0200

    fix: use enum for status

--- lib/presentation/screen/shared/booking_ui_state.dart
+++ lib/presentation/screen/shared/booking_ui_state.dart
@@ -3,28 +3,28 @@
 import 'package:share_space/presentation/screen/shared/workspace_ui_state.dart';
 import 'package:share_space/presentation/util/image_const.dart';
 
-class Booking {
+class BookingUiState {
   final String date;
   final String startTime;
   final String endTime;
-  final bool isUpcoming;
-  final bool isCancelled;
-  final WorkspaceUiState workspaces;
+  final BookingStatus status;
+  final WorkspaceUiState workspace;
 
-  Booking({
+  BookingUiState({
     required this.date,
     required this.startTime,
     required this.endTime,
-    required this.isUpcoming,
-    required this.isCancelled,
-    required this.workspaces,
+    required this.status,
+    required this.workspace,
   }) ;
 
   String getImage() {
     try {
-      return workspaces.imageUrls[0];
+      return workspace.imageUrls[0];
     } catch (e) {
       return FALLBACK_IMAGE;
     }
   }
 }
+
+enum BookingStatus { upcoming, completed, canceled }
