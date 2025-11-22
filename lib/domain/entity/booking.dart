c1658bf84f0a4e681de591ca253926c66d9c35a3
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-11-22 22:42:28 +0200

    feat: booking entity

---
+++ lib/domain/entity/booking.dart
@@ -0,0 +1,30 @@
+import 'package:share_space/domain/entity/workspace.dart';
+
+class Booking {
+  final String id;
+  final String workspaceId;
+  final String userId;
+  final DateTime startTime;
+  final DateTime endTime;
+  final double duration;
+  final String paymentType;
+  final double cost;
+  final String status;
+  final String paymentStatus;
+
+
+  Booking({
+    required this.id,
+    required this.workspaceId,
+    required this.userId,
+    required this.startTime,
+    required this.endTime,
+    required this.duration,
+    required this.paymentType,
+    required this.cost,
+    required this.status,
+    required this.paymentStatus,
+  });
+}
+
+enum BookingStatus { upcoming, completed, canceled }
