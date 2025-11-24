6e542fcca4a372e4588c52cd5e258ad5f152a4fb
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-11-24 18:50:07 +0200

    feat: wrap booking history screen with bloc provider

--- lib/presentation/screen/booking_history/booking_history_screen.dart
+++ lib/presentation/screen/booking_history/booking_history_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart' show BlocBuilder, BlocProvider;
+import 'package:share_space/di/injection.dart';
 import 'package:share_space/presentation/design_system/theme/app_theme.dart';
 import 'package:share_space/presentation/design_system/widget/booking_card/booking_card.dart';
 import 'package:share_space/presentation/design_system/widget/category_chip.dart';
+import 'package:share_space/presentation/screen/booking_history/state/booking_history_cubit.dart';
+import 'package:share_space/presentation/screen/booking_history/state/booking_history_state.dart';
 
 class BookingHistoryScreen extends StatefulWidget {
   const BookingHistoryScreen({super.key});
@@ -49,7 +53,12 @@ class _BookingHistoryScreenState extends State<BookingHistoryScreen> {
     ),
   ];
 
-  final List<String> _filterCategories = ["All", "Upcoming", "Completed", "Cancelled"];
+  final List<String> _filterCategories = [
+    "All",
+    "Upcoming",
+    "Completed",
+    "Cancelled",
+  ];
 
   List<BookingCard> get _filteredBookings {
     if (_selectedIndex == 0) {
@@ -62,53 +71,78 @@ class _BookingHistoryScreenState extends State<BookingHistoryScreen> {
   @override
   Widget build(BuildContext context) {
     final theme = AppTheme.of(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Booking History",
-          style: theme.typography.textTheme.headlineSmall?.copyWith(
-            color: theme.colors.title,
-          ),
-        ),
-        backgroundColor: theme.colors.surface,
-        elevation: 0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          children: [
-            const SizedBox(height: 16),
-            SizedBox(
-              height: 40,
-              child: ListView.separated(
-                scrollDirection: Axis.horizontal,
-                itemCount: _filterCategories.length,
-                itemBuilder: (context, index) {
-                  return CategoryChip(
-                    label: _filterCategories[index],
-                    isSelected: _selectedIndex == index,
-                    onTap: () {
-                      setState(() {
-                        _selectedIndex = index;
-                      });
-                    },
-                  );
-                },
-                separatorBuilder: (context, index) => const SizedBox(width: 8),
+    return BlocProvider(
+      create: (context) => getIt<BookingHistoryCubit>()..fetchBookingHistory(),
+      child: BlocBuilder<BookingHistoryCubit, BookingHistoryState>(
+        builder: (context, state) {
+          if (state is BookingHistoryLoading) {
+            return Center(
+              child: CircularProgressIndicator(
+                color: AppTheme.of(context).colors.primary,
               ),
-            ),
-            const SizedBox(height: 16),
-            Expanded(
-              child: ListView.separated(
-                itemCount: _filteredBookings.length,
-                itemBuilder: (context, index) {
-                  return _filteredBookings[index];
-                },
-                separatorBuilder: (context, index) => const SizedBox(height: 12),
+            );
+          } else if (state is BookingHistoryLoaded) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text(
+                  "Booking History",
+                  style: theme.typography.textTheme.headlineSmall?.copyWith(
+                    color: theme.colors.title,
+                  ),
+                ),
+                backgroundColor: theme.colors.surface,
+                elevation: 0,
               ),
-            ),
-          ],
-        ),
+              body: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 16),
+                    SizedBox(
+                      height: 40,
+                      child: ListView.separated(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: _filterCategories.length,
+                        itemBuilder: (context, index) {
+                          return CategoryChip(
+                            label: _filterCategories[index],
+                            isSelected: _selectedIndex == index,
+                            onTap: () {
+                              setState(() {
+                                _selectedIndex = index;
+                              });
+                            },
+                          );
+                        },
+                        separatorBuilder: (context, index) =>
+                            const SizedBox(width: 8),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Expanded(
+                      child: ListView.separated(
+                        itemCount: _filteredBookings.length,
+                        itemBuilder: (context, index) {
+                          return _filteredBookings[index];
+                        },
+                        separatorBuilder: (context, index) =>
+                            const SizedBox(height: 12),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          } else if (state is BookingHistoryError) {
+            return Center(
+              child: Text(
+                "Please Check your internet connection", //state.message,
+                style: theme.typography.textTheme.labelSmall,
+              ),
+            );
+          }
+          return const SizedBox.shrink();
+        },
       ),
     );
   }
