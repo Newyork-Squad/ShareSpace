255a7a1f58d10f4c3bae22941eab82b11d4b0198
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-11-24 22:05:09 +0200

    feat: remove hardcoded values

--- lib/presentation/screen/booking_history/booking_history_screen.dart
+++ lib/presentation/screen/booking_history/booking_history_screen.dart
@@ -4,11 +4,11 @@ import 'package:share_space/di/injection.dart';
 import 'package:share_space/presentation/design_system/theme/app_theme.dart';
 import 'package:share_space/presentation/design_system/widget/booking_card/booking_card.dart';
 import 'package:share_space/presentation/design_system/widget/category_chip.dart';
-import 'package:share_space/presentation/design_system/widget/error_screen.dart';
 import 'package:share_space/presentation/design_system/widget/loading_screen.dart';
 import 'package:share_space/presentation/screen/booking_history/state/booking_history_cubit.dart';
 import 'package:share_space/presentation/screen/booking_history/state/booking_history_state.dart';
 import 'package:share_space/presentation/screen/shared/ui_state/booking_ui_state.dart';
+import 'package:share_space/presentation/screen/shared/ui_state/workspace_ui_state.dart';
 
 class BookingHistoryScreen extends StatefulWidget {
   const BookingHistoryScreen({super.key});
@@ -20,52 +20,10 @@ class BookingHistoryScreen extends StatefulWidget {
 class _BookingHistoryScreenState extends State<BookingHistoryScreen> {
   int _selectedIndex = 0;
 
-  final List<BookingCard> _allBookings = [
-    BookingCard(
-      imageUrl: "https://example.com/image.png",
-      rating: 4.5,
-      title: "Night Owl Room",
-      price: "12,000 IQD/h",
-      location: "Baghdad, Iraq",
-      amenities: const ["WiFi", "A/C", "Whiteboard", "Power Backup"],
-      status: BookingStatusUiState.upcoming,
-      date: "27 Jun 2025",
-      time: "03:00 PM -> 04:00 PM",
-    ),
-    BookingCard(
-      imageUrl: "https://example.com/image.png",
-      rating: 4.8,
-      title: "The Sunshine Room",
-      price: "15,000 IQD/h",
-      location: "Erbil, Iraq",
-      amenities: const ["WiFi", "A/C", "Projector"],
-      status: BookingStatusUiState.completed,
-      date: "15 May 2025",
-      time: "10:00 AM -> 12:00 PM",
-    ),
-    BookingCard(
-      imageUrl: "https://example.com/image.png",
-      rating: 4.2,
-      title: "The Focus Zone",
-      price: "10,000 IQD/h",
-      location: "Baghdad, Iraq",
-      amenities: const ["WiFi", "Power Backup"],
-      status: BookingStatusUiState.canceled,
-      date: "20 Jun 2025",
-      time: "01:00 PM -> 02:00 PM",
-    ),
-  ];
-
-  final List<String> _filterCategories = ["All"] + BookingStatusUiState.values.map((e) => e.name).toList();
-
-
-  List<BookingCard> get _filteredBookings {
-    if (_selectedIndex == 0) {
-      return _allBookings;
-    }
-    final status = BookingStatusUiState.values[_selectedIndex - 1];
-    return _allBookings.where((booking) => booking.status == status).toList();
-  }
+  final List<String> _filterCategories = [
+        "All"
+      ] +
+      BookingStatusUiState.values.map((e) => e.name).toList();
 
   @override
   Widget build(BuildContext context) {
@@ -75,12 +33,16 @@ class _BookingHistoryScreenState extends State<BookingHistoryScreen> {
       child: BlocBuilder<BookingHistoryCubit, BookingHistoryState>(
         builder: (context, state) {
           if (state is BookingHistoryLoading) {
-            return LoadingScreen();
+            return const LoadingScreen();
           } else if (state is BookingHistoryLoaded) {
+            final allBookings =
+                state.upcoming + state.completed + state.cancelled;
+            final filteredBookings = _getFilteredBookings(allBookings);
+
             return Scaffold(
               backgroundColor: theme.colors.surface,
               appBar: AppBar(
-                leading: SizedBox.shrink(),
+                leading: const SizedBox.shrink(),
                 leadingWidth: 0,
                 title: Text(
                   "Booking History",
@@ -120,9 +82,22 @@ class _BookingHistoryScreenState extends State<BookingHistoryScreen> {
                     const SizedBox(height: 16),
                     Expanded(
                       child: ListView.separated(
-                        itemCount: _filteredBookings.length,
+                        itemCount: filteredBookings.length,
                         itemBuilder: (context, index) {
-                          return _filteredBookings[index];
+                          final booking = filteredBookings[index];
+                          return BookingCard(
+                            imageUrl: booking.getImage(),
+                            rating: booking.workspace.rate,
+                            title: booking.workspace.name,
+                            price: "${booking.workspace.pricePerHour}/h",
+                            location: booking.workspace.locationName,
+                            amenities: booking.workspace.services
+                                .map((service) => service.toString())
+                                .toList(),
+                            status: booking.status,
+                            date: booking.date,
+                            time: "${booking.startTime} -> ${booking.endTime}",
+                          );
                         },
                         separatorBuilder: (context, index) =>
                             const SizedBox(height: 12),
@@ -139,11 +114,18 @@ class _BookingHistoryScreenState extends State<BookingHistoryScreen> {
                 style: theme.typography.textTheme.labelSmall,
               ),
             );
-            // return ErrorScreen(hasAppBar: false);
           }
           return const SizedBox.shrink();
         },
       ),
     );
+  }
+
+  List<BookingUiState> _getFilteredBookings(List<BookingUiState> allBookings) {
+    if (_selectedIndex == 0) {
+      return allBookings;
+    }
+    final status = BookingStatusUiState.values[_selectedIndex - 1];
+    return allBookings.where((booking) => booking.status == status).toList();
   }
 }
