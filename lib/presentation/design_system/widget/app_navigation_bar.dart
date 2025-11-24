ebb214e3cba352cde0d23983ba2ca4b945d4f60a
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-11-24 22:44:26 +0200

    fix: booking history import in navigation bar

--- lib/presentation/design_system/widget/app_navigation_bar.dart
+++ lib/presentation/design_system/widget/app_navigation_bar.dart
@@ -5,7 +5,7 @@ import 'package:share_space/presentation/screen/home/home_screen.dart';
 import 'package:share_space/presentation/screen/my_account/my_account_screen.dart';
 import 'package:share_space/presentation/screen/search/search_screen.dart';
 
-import '../screen/booking_history/booking_history_screen.dart';
+import '../../screen/booking_history/booking_history_screen.dart';
 import '../theme/app_theme.dart';
 
 class AppNavigationBar extends StatefulWidget {
