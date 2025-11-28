0bb147d379f56c8103015bbf16f727c6cff12328
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-11-28 17:04:30 +0200

    fix: wrap with material

--- lib/presentation/screen/search/search_screen.dart
+++ lib/presentation/screen/search/search_screen.dart
@@ -23,98 +23,105 @@ class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
     final theme = AppTheme.of(context);
-    return BlocProvider(
-      create: (context) => getIt<SearchCubit>()..fetchLastViewed(),
-      child: BlocBuilder<SearchCubit, SearchState>(
-        builder: (context, state) {
-          if (state is SearchLoading) {
-            return LoadingScreen();
-          } else if (state is SearchLoaded) {
-            final rooms = (state.searchResults.isEmpty)
-                ? state.lastViewed
-                : state.searchResults;
+    return Material(
+      color: theme.colors.surfaceLow,
+      child: BlocProvider(
+        create: (context) => getIt<SearchCubit>()..fetchLastViewed(),
+        child: BlocBuilder<SearchCubit, SearchState>(
+          builder: (context, state) {
+            if (state is SearchLoading) {
+              return LoadingScreen();
+            } else if (state is SearchLoaded) {
+              final rooms = (state.searchResults.isEmpty)
+                  ? state.lastViewed
+                  : state.searchResults;
 
-            return CustomScrollView(
-              slivers: [
-                SliverAppBar(
-                  leading: const SizedBox.shrink(),
-                  leadingWidth: 0,
-                  title: Text(
-                    AppStrings.searchScreenTitle,
-                    style: theme.typography.textTheme.titleMedium?.copyWith(
-                      color: theme.colors.title,
-                    ),
-                  ),
-                  backgroundColor: theme.colors.surface,
-                  surfaceTintColor: theme.colors.surface,
-                  elevation: 0,
-                ),
-                SliverToBoxAdapter(
-                  child: SearchField(suggestions: [], history: []),
-                ),
-                SliverToBoxAdapter(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 12,
-                    ),
-                    child: Text(
-                      (state.searchResults.isEmpty)
-                          ? AppStrings.mostSearched
-                          : AppStrings.resultsFound(state.searchResults.length),
-                      style: theme.typography.textTheme.titleSmall?.copyWith(
+              return CustomScrollView(
+                slivers: [
+                  SliverAppBar(
+                    leading: const SizedBox.shrink(),
+                    leadingWidth: 0,
+                    title: Text(
+                      AppStrings.searchScreenTitle,
+                      style: theme.typography.textTheme.titleMedium?.copyWith(
                         color: theme.colors.title,
                       ),
                     ),
+                    backgroundColor: theme.colors.surface,
+                    surfaceTintColor: theme.colors.surface,
+                    elevation: 0,
                   ),
-                ),
-                SliverToBoxAdapter(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: rooms
-                        .map(
-                          (room) => Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 16),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                InkWell(
-                                  onTap: () {
-                                    Navigator.pushNamed(
-                                      context,
-                                      Routes.roomDetailsScreen,
-                                      arguments: room.id.toString(),
-                                    );
-                                  },
-                                  child: WorkspaceDetailsCard(
-                                    title: room.name,
-                                    imageUrl: room.imageUrls.isNotEmpty
-                                        ? room.imageUrls[0]
-                                        : '',
-                                    rating: room.rate,
-                                    price:
-                                        "${room.pricePerHour.toInt().toString()}/h",
-                                    location: room.locationName,
-                                    amenities: room.services
-                                        .map((e) => e.toString())
-                                        .toList(),
+                  SliverToBoxAdapter(
+                    child: SearchField(suggestions: [], history: []),
+                  ),
+                  SliverToBoxAdapter(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 12,
+                      ),
+                      child: Text(
+                        (state.searchResults.isEmpty)
+                            ? AppStrings.mostSearched
+                            : AppStrings.resultsFound(
+                                state.searchResults.length,
+                              ),
+                        style: theme.typography.textTheme.titleSmall?.copyWith(
+                          color: theme.colors.title,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SliverToBoxAdapter(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: rooms
+                          .map(
+                            (room) => Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 16,
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  InkWell(
+                                    onTap: () {
+                                      Navigator.pushNamed(
+                                        context,
+                                        Routes.roomDetailsScreen,
+                                        arguments: room.id.toString(),
+                                      );
+                                    },
+                                    child: WorkspaceDetailsCard(
+                                      title: room.name,
+                                      imageUrl: room.imageUrls.isNotEmpty
+                                          ? room.imageUrls[0]
+                                          : '',
+                                      rating: room.rate,
+                                      price:
+                                          "${room.pricePerHour.toInt().toString()}/h",
+                                      location: room.locationName,
+                                      amenities: room.services
+                                          .map((e) => e.toString())
+                                          .toList(),
+                                    ),
                                   ),
-                                ),
-                                const SizedBox(height: 12),
-                              ],
+                                  const SizedBox(height: 12),
+                                ],
+                              ),
                             ),
-                          ),
-                        )
-                        .toList(),
+                          )
+                          .toList(),
+                    ),
                   ),
-                ),
-              ],
-            );
-          } else if (state is SearchError) {
-            return const ErrorScreen(hasAppBar: false);
-          }
-          return const SizedBox.shrink();
-        },
+                ],
+              );
+            } else if (state is SearchError) {
+              return const ErrorScreen(hasAppBar: false);
+            }
+            return const SizedBox.shrink();
+          },
+        ),
       ),
     );
   }
